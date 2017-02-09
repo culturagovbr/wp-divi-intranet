353c6c92c9b28c35a21efd069da32feba805d2e0
Fernão Lopes <fernao@riseup.net>
2017-02-09 18:22:52 -0200

    alterando divi para para esquema de css versionavel

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+// adicionando css que faz a função do divi custom css (Carrega por último)
 function divi_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/custom.css');
 }
+
+add_action( 'wp_head', 'divi_child_enqueue_styles' );
+
 ?>
