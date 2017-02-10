989c7ad866ade333ecc7000a399c81ec3bc4c803
Fernão Lopes <fernao@riseup.net>
2017-02-10 16:13:43 -0200

    adicionando classe de formatação em página de categoria

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div id="content-area" class="clearfix">
 			<div id="left-area">
-    <h1><?php single_cat_title(); ?></h1>
+    <h1 class="titulo-pagina-interna"><?php single_cat_title(); ?></h1>
 		<?php
 			if ( have_posts() ) :
 				while ( have_posts() ) : the_post();
